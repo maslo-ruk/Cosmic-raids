f665b33053e4a86609a5b304455d47fa3e2df301
Egor <maslo.paslo@yandex.ru>
2024-12-16 22:37:38 +0300

    начало

--- main.py
+++ main.py
@@ -1,38 +1,15 @@
-# Example file showing a circle moving on screen
-# Примеры событий
-# python -m pygame.examples.eventlist
-
 import pygame
+from data.platformer import Platformer
 
-# pygame setup
-pygame.init()
-size = width, height = 1280, 720
-screen = pygame.display.set_mode(size)
-clock = pygame.time.Clock()
-pygame.display.set_caption("Белий кролик")
-running = True
-MYEVENTTYPE = pygame.USEREVENT + 1
-pygame.time.set_timer(MYEVENTTYPE, 1000)
+def main():
+    pygame.init()
+    size = width, height = 900, 600
+    screen = pygame.display.set_mode(size)
+    clock = pygame.time.Clock()
+    pygame.display.set_caption("CosmicRaids")
+    current_scene = Platformer(size, screen, clock)
+    runi = True
+    while runi:
+        current_scene.run()
 
-while running:
-    screen.fill("purple")
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEMOTION:
-            pygame.draw.circle(screen, 'white', event.pos, 20)
-            pygame.draw.ellipse(screen, 'white', (event.pos[0] - 15,
-                                                  event.pos[1] - 40, 10, 40), 20)
-            pygame.draw.ellipse(screen, 'white', (event.pos[0] + 5,
-                                                  event.pos[1] - 40, 10, 40), 20)
-        if event.type == MYEVENTTYPE:
-            print("Мое событие")
 
-    pygame.display.flip()
-    # limits FPS to 60
-    # independent physics.
-    dt = clock.tick(30) / 1000
-
-pygame.quit()
