1de09ac25fd040899389e3520eaa37c5b82ec2b4
Александр <a.kononov9@yandex.ru>
2024-12-16 17:42:43 +0300

    Кононов Александр: Все вместе

--- data/platformer.py
+++ data/platformer.py
@@ -14,19 +14,32 @@ clock = pygame.time.Clock()
 pygame.display.set_caption("shariki")
 running = True
 
+
+class Bullets(pygame.sprite.Sprite):
+    def __init__(self, start_pos, angle):
+        super().__init__()
+        self.image = pygame.Surface((5, 5))
+        self.image.fill('red')
+        self.rect = self.image.get_rect(center=start_pos)
+        self.angle = angle
+
+    def update(self):
+        # Двигаем линию в зависимости от угла
+        self.rect.x += 10 * self.angle[0]
+        self.rect.y += 10 * self.angle[1]
+
+        # Удаляем линию, если она выходит за границы экрана
+        if self.rect.x < 0 or self.rect.x > 1280 or self.rect.y < 0 or self.rect.y > 720:
+            self.kill()
+
+
+all_b = pygame.sprite.Group()
+lines = pygame.sprite.Group()
 player = Player()
 right = False
 left = False
 up = False
 while running:
-    tick = clock.tick(60)
-    screen.fill('blue')
-    for rect in rects:
-        pygame.draw.rect(screen, 'green', rect)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
     keys = pygame.key.get_pressed()
     if keys[pygame.K_d]:
         right = True
@@ -40,6 +53,30 @@ while running:
         up = True
     else:
         up = False
+    # tick = clock.tick(60)
+    screen.fill('blue')
+    for rect in rects:
+        pygame.draw.rect(screen, 'green', rect)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                dx = mouse_x - player.rect.x
+                dy = mouse_y - player.rect.y
+                angle = (dx, dy)
+                if dx != 0 or dy != 0:  # Проверка для избежания деления на ноль
+                    norm = (dx ** 2 + dy ** 2) ** 0.5
+                    direction = (dx / norm, dy / norm)
+                    line = Bullets(player.rect.center, direction)
+                    all_b.add(line)
+                    lines.add(line)
+
+
     player.update(right, left, up, rects)
+    all_b.draw(screen)
     screen.blit(player.image, (player.rect.x, player.rect.y))
+    lines.update()
     pygame.display.flip()
+    tick = clock.tick(60)
