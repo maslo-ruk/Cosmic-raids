bd1720d9de6c6aa74d8322714814daef1162e9e4
Helen_Kulpinsky <e.kulpinskaya@yandex.ru>
2025-01-09 21:22:21 +0300

    + текстурки хаба

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from data.menu_test_file import Menu
 from data.sound_function import sound
+from data.Corabl_test import Hub
 
 if __name__ == '__main__':
     pygame.init()
@@ -14,7 +15,7 @@ if __name__ == '__main__':
     screen = pygame.display.set_mode((width, height))
     clock = pygame.time.Clock()
     pygame.display.set_caption("Тестовое меню")
-    menu_ecr = Menu(width, height, screen)
+    menu_ecr = Hub(width, height, screen)
     running = True
     while running:
         menu_ecr.run()
