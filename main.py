57aa08ad2f20a0f52e492bbca4ce4d9cb937850b
Egot_Zhavoronkov <georgelarkenston@gmail.com>
2025-01-25 21:48:36 +0300

    рабочая версия

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def main_lena():
 
 def main():
     pygame.init()
-    size = width, height = 2500, 900
+    size = width, height = 1500, 900
     screen = pygame.display.set_mode(size)
     clock = pygame.time.Clock()
     pygame.display.set_caption("CosmicRaids")
