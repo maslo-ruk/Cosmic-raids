92a28216806093b9f355c44c873ebaada888058f
Helen_Kulpinsky <e.kulpinskaya@yandex.ru>
2025-02-03 05:51:33 +0300

    дабавленна пасхалка

--- hub.py
+++ hub.py
@@ -5,6 +5,7 @@ from data.end_of_testfile import The_end
 from data.settings import Settings
 from data.platformer import *
 from data.test_file_delite import *
+from data.gildia_paskalka_test_file import *
 def main():
     pygame.init()
     screen_info = pygame.display.Info()
@@ -14,7 +15,7 @@ def main():
     screen = pygame.display.set_mode(size)
     clock = pygame.time.Clock()
     pygame.display.set_caption("CosmicRaids")
-    menu_ecr = Pausa_lol((width, height), screen, clock)
+    menu_ecr = Gildia_paskalka(width, height, screen)
     running = True
     while running:
         menu_ecr.run()
